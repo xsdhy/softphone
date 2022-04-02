a27edb3aa897e96df256dc80f50f1a80d0e5951f
唐川 <tangchuan@geotmt.com>
2022-04-02 18:39:52 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
     mode:"none",
     output: {
@@ -13,5 +16,18 @@ module.exports = {
         },
         compress: true,
         port: 9000,
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+                { from: 'static', to: path.resolve(__dirname, 'dist/static') }
+            ],
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            scriptLoading:'blocking',
+        }),
+
+    ],
 }
