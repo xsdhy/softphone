c3b1de9b08af6510d3fb0b1f22138ecf8ef88345
xsdhy <402837074@qq.com>
2026-01-28 16:15:51 +0800

    refactor: update external script sources and enhance SipCall with AutoCallTask as a static property

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import SipCall from './sipcall';
 import { AutoCallTask } from './auto_call_task';
 
-export { SipCall, AutoCallTask };
+// 将 AutoCallTask 挂载为 SipCall 的静态属性
+// 使用方式：new SipCall.AutoCallTask(sipClient, config, callback)
+(SipCall as any).AutoCallTask = AutoCallTask;
+
+// 只使用默认导出，确保 UMD 模式下 window.SipCall 直接是 SipCall 类
+// 这样原有的 new SipCall(...) 和 SipCall.getMediaDeviceInfo() 继续正常工作
 export default SipCall; 
